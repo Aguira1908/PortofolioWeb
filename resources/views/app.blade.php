0bd02af2196404a7fc6ea1825fee10dae3228ecc
Aguira1908 <zikkriamri123@gmail.com>
2025-11-28 12:12:33 +0700

    firstcommit/setup-project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="ie=edge" http-equiv="X-UA-Compatible">
+  <title>Portofolio</title>
+  @vite('resources/css/app.css')
+
+  @viteReactRefresh
+  @vite('resources/js/app.jsx')
+  @inertiaHead
+</head>
+
+<body>
+  @inertia
+</body>
+
+</html>
